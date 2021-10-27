3cc4f93e30265088124636fdc1e79ef3e7038c3f
johnhowelluk <37045219+johnhowelluk@users.noreply.github.com>
2021-10-27 18:05:55 +0100

    Update const.py

--- const.py
+++ const.py
@@ -1,6 +1,6 @@
 """Constants for integration."""
 
-DOMAIN = "gogogate2"
+DOMAIN = "ggg2custom"
 DATA_UPDATE_COORDINATOR = "data_update_coordinator"
 DEVICE_TYPE_GOGOGATE2 = "gogogate2"
 DEVICE_TYPE_ISMARTGATE = "ismartgate"
